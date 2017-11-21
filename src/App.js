0628338582618edd564c752c6931c096d1d3987b
LATAM\filhol <laercio.filho@wunderman.com>
2017-11-21 19:08:29 -0200

    estilizando box do app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Lista from './componentes/lista/lista' ;
+import Filtro from './componentes/filtro/filtro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Filtro/>
         <Lista/>
       </div>
     );
---
+++ src/componentes/filtro/filtro.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Filtro extends Component{
+    render(){
+        return(
+            <div className="filtro_app">
+                <p>Filtro</p>
+            </div>
+        )
+    }
+}
+export default Filtro;
